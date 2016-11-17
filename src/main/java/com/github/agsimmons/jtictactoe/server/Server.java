2f56e3d8c763e8c96ba7d69ba51f31fdfa8cedd4
Andrew Simmons <agsimmons0@gmail.com>
2016-11-16 19:23:23 -0500

    Change name in copyright notices

--- src/main/java/com/github/agsimmons/jtictactoe/server/Server.java
+++ src/main/java/com/github/agsimmons/jtictactoe/server/Server.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2016 andrew.
+ * Copyright 2016 Andrew Simmons
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
