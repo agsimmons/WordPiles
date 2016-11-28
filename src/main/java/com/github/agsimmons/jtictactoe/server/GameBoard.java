be6485040fe64d2a1df9f4868732867b1830f6ed
Andrew Simmons <agsimmons0@gmail.com>
2016-11-27 20:19:01 -0500

    Remove unnecessary whitespace in GameBoard.java

--- src/main/java/com/github/agsimmons/jtictactoe/server/GameBoard.java
+++ src/main/java/com/github/agsimmons/jtictactoe/server/GameBoard.java
@@ -1,22 +1,22 @@
 package com.github.agsimmons.jtictactoe.server;
 
 public class GameBoard {
-    
+
     private final int DEFAULT_DIMENSION = 3;
-    
+
     private int[][] board;
-    
+
     public GameBoard() {
         board = new int[DEFAULT_DIMENSION][DEFAULT_DIMENSION];
     }
-    
+
     public GameBoard(int dimension) {
         board = new int[dimension][dimension];
     }
-    
+
     public String drawBoardState() {
         String boardState = " 012\n";
-        
+
         for (int y = 0; y < board.length; y++) {
             boardState += y;
             for (int x = 0; x < board.length; x++) {
@@ -34,16 +34,16 @@ public class GameBoard {
             }
             boardState += "\n";
         }
-        
+
         return boardState;
     }
-    
+
     public void makeMove(int player, int x, int y) {
         board[x][y] = player;
     }
-    
+
     public int checkForWinner() {
-        
+
         // Player One
         // Horizontal
         if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1) {
@@ -67,7 +67,7 @@ public class GameBoard {
         } else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1) {
             return 1;
         }
-        
+
         // Player Two
         // Horizontal
         if (board[0][0] == 2 && board[1][0] == 2 && board[2][0] == 2) {
@@ -91,7 +91,7 @@ public class GameBoard {
         } else if (board[2][0] == 2 && board[1][1] == 2 && board[0][2] == 2) {
             return 2;
         }
-        
+
         return -1;
     }
 }
